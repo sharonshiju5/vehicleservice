6623b38ba1f93f3ed61aa97aef38be9a9c1ec93b
vishag10 <vishagchandran10.com>
2025-09-09 20:29:27 +0530

    build error

--- src/app/oneservicedeatils/[id]/page.tsx
+++ src/app/oneservicedeatils/[id]/page.tsx
@@ -3,11 +3,7 @@ import React, { useState, useEffect } from 'react'
 import App from './components/App'
 import DeskTop from './components/Desktop'
 
-interface PageProps {
-  params: Promise<{ id: string }>
-}
-
-function Page({ params }: PageProps) {
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const [isMobile, setIsMobile] = useState(false)
   const [id, setId] = useState<string>('')
   
@@ -34,5 +30,3 @@ function Page({ params }: PageProps) {
     </div>
   )
 }
-
-export default Page
